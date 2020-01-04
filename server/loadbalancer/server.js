aebebb07812cbe83ef7307939b30dcdf1c431841
Micah <micahdshackelford@gmail.com>
2020-01-03 20:19:26 -0500

    fix loadbal

--- server/loadbalancer/server.js
+++ server/loadbalancer/server.js
@@ -6,22 +6,20 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-let proxies = [
+const proxies = [
   'http://172.31.23.113:3001/',
   'http://172.31.13.101:3001/',
   'http://172.31.34.65:3001/',
   'http://172.31.39.69:3001/',
   'http://172.31.35.18:3001/',
-];
+]
 
 const balancer = new loadbalancer.P2cBalancer(proxies.length);
 
-app.all('/', (req,res,next) => {
+
+app.all('/', () => {
   const target = proxies[balancer.pick()];
-
-  proxy('/', { target });
-
-  res.end();
+  proxy('/', { target })
 });
 
 app.listen(PORT,() => {
